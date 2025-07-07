d655a18204e07013d209b0c4e8de3eddde701e69
Jan Kohlbach <mail@jankohlbach.dev>
2025-07-07 15:42:37 +0200

    🚧 change structure for usage of content collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { defineCollection, z } from 'astro:content';
 
 import { glob } from 'astro/loaders';
 
 const pagesGeneric = defineCollection({
   loader: glob({base: './src/content/pagesGeneric', pattern: '**/*.md'}),
-  schema: z.object({
-    title: z.string(),
-    content: z.string(),
-  }),
 });
 
 export const collections = { pagesGeneric };
--- src/content/pagesGeneric/test-1.md
+++
@@ -1,9 +0,0 @@
----
-slug: test-1
-layout: ../layouts/PageGeneric.astro
-title: Test 1
-sections:
-  - modules:
-      - type: title
-        title: Test 1
----

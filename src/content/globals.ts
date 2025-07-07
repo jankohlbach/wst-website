9862cdce5cc4065fd82f51c65d048fe6883d5c17
Jan Kohlbach <mail@jankohlbach.dev>
2025-07-07 16:08:17 +0200

    ♻️ restructure usage of globals

---
+++ src/content/globals.ts
@@ -0,0 +1,6 @@
+import globals from './globals.json';
+
+export const general = globals.general[0];
+export const header = globals.header[0];
+export const footer = globals.footer[0];
+export const socials = globals.socials[0];

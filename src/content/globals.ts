a0ae0e9130a8de84773af03a64b8737b8c072ea4
Jan Kohlbach <mail@jankohlbach.dev>
2025-07-07 16:14:51 +0200

    ✨ add 404 page settings to globals

--- src/content/globals.ts
+++ src/content/globals.ts
@@ -4,3 +4,4 @@ export const general = globals.general[0];
 export const header = globals.header[0];
 export const footer = globals.footer[0];
 export const socials = globals.socials[0];
+export const page404 = globals.page404[0];

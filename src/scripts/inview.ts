ffcd45d58f98fc606ec0b9ff26fe7e62bd15ff18
Jan Kohlbach <mail@jankohlbach.dev>
2025-07-14 17:37:47 +0200

    📱 intro > finish mobile adjustments

--- src/scripts/inview.ts
+++ src/scripts/inview.ts
@@ -1,6 +1,7 @@
 let inviewTrigger: NodeList | null = null
+let inviewTriggerNoOffset: NodeList | null = null
 
-const initInViewTrigger = () => {
+const initInViewTrigger = ({trigger, offset = false}: {trigger: NodeList, offset: boolean}) => {
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
@@ -10,18 +11,24 @@ const initInViewTrigger = () => {
       })
     },
     {
-      rootMargin: `0% 0% ${window.innerWidth < 768 ? '-10%' : '-20%'} 0%`,
+      rootMargin: `0% 0% ${offset ? (window.innerWidth < 768 ? '-10%' : '-20%') : '0%'} 0%`,
     },
   )
 
-  inviewTrigger?.forEach((elTrigger) => {
+  trigger?.forEach((elTrigger) => {
     observer.observe(elTrigger as HTMLElement)
   })
 }
 
 window.addEventListener('load', () => {
   inviewTrigger = document.querySelectorAll('[data-inview]');
+  inviewTriggerNoOffset = document.querySelectorAll('[data-inview-no-offset]');
+
   if (inviewTrigger) {
-    initInViewTrigger();
+    initInViewTrigger({trigger: inviewTrigger, offset: true});
+  }
+
+  if (inviewTriggerNoOffset) {
+    initInViewTrigger({trigger: inviewTriggerNoOffset, offset: false});
   }
 });

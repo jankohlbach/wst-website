ed09c2f97cbba359f2cae1770375f1eb76ac9fa1
Jan Kohlbach <mail@jankohlbach.dev>
2026-03-19 16:55:38 +0100

    ✨ anonymize content a bit

---
+++ src/scripts/consoleCredits.ts
@@ -0,0 +1,9 @@
+console.log(
+  '%c⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡\n⟡                                   ⟡\n⟡  site by https://jankohlbach.com  ⟡\n⟡                                   ⟡\n⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡⟡',
+  `
+    font-family: monospace;
+    font-size: 14px;
+    color: #F4CBDF;
+    background: #152720;
+  `,
+);
